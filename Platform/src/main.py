eb06137dcf080b907956bb4c74baf496bf3e3b81
Iqbal <mahammad@buffalo.edu>
2025-04-24 00:41:04 -0400

    main: updated main file with clean imports using dependencies from src.core

--- Platform/src/main.py
+++ Platform/src/main.py
@@ -1,22 +1,15 @@
 import logging
-from contextlib import asynccontextmanager
 
 from fastapi import FastAPI, Request, status
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 from Platform.src.user_management.router import router as user_management_router
-from Platform.src.config.logging_config import configure_logging
+from Platform.src.core.lifespan import lifespan
 
 from asgi_correlation_id import CorrelationIdMiddleware
 
 from Platform.src.user_management.exceptions import *
 logger = logging.getLogger(__name__)
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-
-    configure_logging()
-    yield
 
 app = FastAPI(lifespan=lifespan)
 
