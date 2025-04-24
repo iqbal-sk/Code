0edab3f6c8ecd5b7b8c5ace8e73a0cd991ad8c8a
D-Siri <siriduggineni01@gmail.com>
2025-04-24 16:51:21 -0400

    models: add TestCase ODMantic model for test cases

---
+++ Platform/src/problem_management/models/testcase.py
@@ -0,0 +1,27 @@
+from pydantic import ConfigDict
+from datetime import datetime
+from typing import Optional
+from bson import ObjectId
+from odmantic import Model, Field
+
+class FileReferences(Model):
+    inputFileId: ObjectId
+    outputFileId: ObjectId
+
+    model_config = ConfigDict(
+        arbitrary_types_allowed=True
+    )
+
+class TestCase(Model):
+    pId: str
+    input: Optional[str] = None
+    expectedOutput: Optional[str] = None
+    isHidden: bool = False
+    isLargeFile: bool = False
+    fileReferences: Optional[FileReferences] = None
+    createdAt: datetime = Field(default_factory=datetime.utcnow)
+
+    model_config = ConfigDict(
+        arbitrary_types_allowed=True,
+        collection="test_cases"
+    )
